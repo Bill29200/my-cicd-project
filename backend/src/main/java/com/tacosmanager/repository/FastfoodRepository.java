43009763e655e58d028b19efceb063fbb6cadee4
NABIL KADJOUH <96910658+Bill29200@users.noreply.github.com>
2025-12-17 19:53:16 +0100

    Update FastfoodRepository.java

--- backend/src/main/java/com/tacosmanager/repository/FastfoodRepository.java
+++ backend/src/main/java/com/tacosmanager/repository/FastfoodRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface FastfoodRepository extends JpaRepository<Fastfood, Long> {
 
     Optional<Fastfood> findById(Long id);
